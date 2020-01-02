a7b884d8a35d57459d2a0a8d7a6d0883f402e54e
vincecachi <vincentcachila@gmail.com>
2020-01-02 09:31:27 +0800

    asd

--- invoice-add.php
+++ invoice-add.php
@@ -25,10 +25,10 @@ if(isset($_POST['fullypaid'])){
     $y = date('y');
     $d = date('d');
 
-    $qry = mysqli_query($link,"SELECT MAX(outbound_ID) FROM 'outboundtb' "); // Get the latest ID
+    $qry = mysqli_query($link,"SELECT MAX(id) FROM `outboundtb` "); // Get the latest ID
     $resulta = mysqli_fetch_array($qry);
-    $newID = $resulta['MAX(outbound_ID)'] + 1; //Get the latest ID then Add 1
-    $custID = str_pad($newID, 5, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
+    $newID = $resulta['MAX(id)'] + 1; //Get the latest ID then Add 1
+    $custID = str_pad($newID, 8, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
     $obtxid = $IDtype.$custID; //Prepare custom ID
 
 
@@ -113,10 +113,10 @@ if(isset($_POST['fullypaid'])){
         $y = date('y');
         $d = date('d');
 
-        $qry = mysqli_query($link,"SELECT MAX(outbound_ID) FROM 'outboundtb' "); // Get the latest ID
+        $qry = mysqli_query($link,"SELECT MAX(id) FROM `outboundtb` "); // Get the latest ID
         $resulta = mysqli_fetch_array($qry);
-        $newID = $resulta['MAX(outbound_ID)'] + 1; //Get the latest ID then Add 1
-        $custID = str_pad($newID, 5, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
+        $newID = $resulta['MAX(id)'] + 1; //Get the latest ID then Add 1
+        $custID = str_pad($newID, 8, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
         $obtxid = $IDtype.$custID; //Prepare custom ID
 
         //insert query to outboundTB table
