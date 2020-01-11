bf8abba7a42b2fae449cc752adab1930ecf38d4a
vincecachi <vincentcachila@gmail.com>
2020-01-11 18:12:27 +0800

    asd

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -35,7 +35,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="invoice-add.php" class="nav-link">
+                <a href="invoice-add-2.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Add Invoice</p>
                 </a>
