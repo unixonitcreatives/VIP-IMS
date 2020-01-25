ff4a067b5944b44bfe2f1e491426461773f194a8
Joff <joffpascual@gmail.com>
2020-01-25 10:32:43 +0800

    dasdasd
    asdasdas

--- index.php
+++ index.php
@@ -40,9 +40,73 @@
            <div class="col-lg-12">
             <div class="card">
               <div class="card-header border-0">
-              <h3>Welcome to VIP Inventory Management Dashboard! BETA TESTING</h3> 
+              <h3>Welcome to VIP Inventory Management Dashboard! BETA TESTING</h3>
               </div>
+
               <div class="card-body">
+              <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
+
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Bounce Rate</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>User Registrations</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Unique Visitors</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <hr>
               <label>Quick Access:</label><br>
               <a class="btn btn-primary" href="members-add.php"><i class="nav-icon fas fa-users"></i> New Member</a>
               <a class="btn btn-primary" href="product-add.php"><i class="nav-icon fas fa-cubes"></i> New Product Model</a>
--- package-update.php
+++ package-update.php
@@ -1,248 +1,253 @@
-<?php include "session.php"; ?>
-<?php include "config.php"; ?>
+  <?php include "session.php"; ?>
+  <?php include "config.php"; ?>
 
-<?php
+  <?php
 
-$id = $_GET['custID'];
+  $id = $_GET['custID'];
 
-// Attempt select query execution
-$Getquery = "SELECT * FROM product_model WHERE custID = '$id'";
-if($Getresult = mysqli_query($link, $Getquery)){
-  if(mysqli_num_rows($Getresult) > 0){
-    while($row = mysqli_fetch_array($Getresult)){
-      $custID = $row['custID'];
-      $description = $row['description'];
-      $sku = $row['sku'];
-      $created_by = $row['created_by'];
-      $created_at = $row['created_at'];
-      $type = $row['type'];
+  // Attempt select query execution
+  $Getquery = "SELECT * FROM product_model WHERE custID = '$id'";
+  if($Getresult = mysqli_query($link, $Getquery)){
+    if(mysqli_num_rows($Getresult) > 0){
+      while($row = mysqli_fetch_array($Getresult)){
+        $custID = $row['custID'];
+        $description = $row['description'];
+        $sku = $row['sku'];
+        $created_by = $row['created_by'];
+        $created_at = $row['created_at'];
+        $type = $row['type'];
 
+      }
+      // Free result set
+      //mysqli_free_result($Getresult);
     }
-    // Free result set
-    //mysqli_free_result($Getresult);
+  } else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
   }
-} else{
-  echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
 
-?>
+  ?>
 
 
 
- <?php
+   <?php //UPDATING PACKAGE
+
+    //If the form is submitted
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+      //post validation
+      $packname = validation($_POST['packName']);
+      $sku = validation($_POST['sku']);
 
 
-  //If the form is submitted
-  if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $packname = validation($_POST['packName']);
-    $sku = validation($_POST['sku']);
+      //if empty required fields
+      if(empty($packname) || empty($sku)){
 
-    //if empty required fields
-    if(empty($packname) || empty($sku)){
+        $alertMessage = "<div class='alert alert-danger' role='alert'>
+            Please input required fields.
+            </div>";
+          }else{
+        //UPDATE query to product_model table
+      $updatePackageQuery = "
+      UPDATE `product_model` SET description = '$packname', sku = '$sku' WHERE custID = '$id' ";
+      $packageResult = mysqli_query($link, $updatePackageQuery) or die(mysqli_error($link));
 
-      $alertMessage = "<div class='alert alert-danger' role='alert'>
-          Please input required fields.
-          </div>";
-        }else{
-      //UPDATE query to packages table
-    $updatePackageQuery = "
-    UPDATE `product_model` SET description = '$packName', sku = '$sku' WHERE custID = '$id' ";
-    $packageResult = mysqli_query($link, $updatePackageQuery) or die(mysqli_error($link));
+      //if update executed
+      if ($packageResult === TRUE) {
 
-    if ($packageResult === TRUE) {
-
-       echo "<script>alert('Insert 1 Updated')</script>";
+         echo "<script>alert('Package Updated')</script>";
 
 
-      $j = 0;
+        //assign to variable
+        //init
+        $j = 0;
 
-      //Counts the elements in array
-      $count = count($_POST['packlist']);
+        //Counts the elements in array
+        $count = count($_POST['packlist']);
+
+        //for loop if packlist rows < 0
+        for ($j = 0; $j < $count; $j++) {
+
+          //Update package_list table
+          $listquery = "UPDATE package_list SET pack_list_model = '".$_POST['packlist'][$j]."', pack_list_qty = '".$_POST['packlistQty'][$j]."' WHERE packId = '$id' ";
+          $listresult = mysqli_multi_query($link, $listquery) or die(mysqli_error($link));
+
+          }
+
+          //if executed
+          if($listresult === TRUE){
+            //logs
+            $info = $_SESSION['username']." update package";
+            $info2 = "Details: ".$packname.", ".$id;
+            $alertlogsuccess = $packname.": has been updated succesfully!";
+            include "logs.php";
+            echo "<script>window.location.href='package-manage.php'</script>";
+                  
+                  // Free result set
+                  //mysqli_free_result($result);
+                } else{
+                  echo "<script>alert('Error creating logs')</script>";
+                }
+
+
+      }// ./pack result
+
+
+      }// ./validation
+
+    }// ./post
+    else{
+            $alertMessage = "<div class='alert alert-danger' role='alert'>
+            Error Creating Package.
+            </div>";
+          }
 
       
-      for ($j = 0; $j < $count; $j++) {
 
-        $listquery = "UPDATE package_list SET pack_list_model = '$packList[$j]', pack_list_qty = '$packQty[$j]' WHERE packId = '$id' ";
-        $listresult = mysqli_multi_query($link, $listquery) or die(mysqli_error($link));
-
+        function validation($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
         }
+        // Close connection
+        //mysqli_close($link);
+        ?>
 
-        if($listresult === TRUE){
-          //logs
-          $info = $_SESSION['username']." update package";
-          $info2 = "Details: ".$packname.", ".$id;
-          $alertlogsuccess = $packname.": has been updated succesfully!";
-          include "logs.php";
-          echo "<script>window.location.href='package-manage.php'</script>";
-                // Free result set
-                //mysqli_free_result($result);
-              } else{
-                echo "<script>alert('Error creating logs')</script>";
-              }
+  <!DOCTYPE html>
+  <html lang="en">
+  <?php include "includes/header.php"; ?>
 
+  <body class="hold-transition sidebar-mini">
+    <div class="wrapper">
 
-    }// ./pack result
+      <?php include "includes/navbar.php"; ?>
+      <?php include "includes/sidebar.php"; ?>
 
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+          <div class="container-fluid">
+            <div class="row mb-2">
+              <div class="col-sm-6">
+                <h1 class="m-0 text-dark">VIP Inventory Management System</h1>
+              </div><!-- /.col -->
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                  <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                  <li class="breadcrumb-item active">View Package # <?php echo $custID; ?></li>
+                </ol>
+              </div><!-- /.col -->
+            </div><!-- /.row -->
+          </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-
-
-    }// ./validation
-
-  }// ./post
-  else{
-          $alertMessage = "<div class='alert alert-danger' role='alert'>
-          Error Creating Package.
-          </div>";
-        }
-
-    
-
-      function validation($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
-      // Close connection
-      //mysqli_close($link);
-      ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<?php include "includes/header.php"; ?>
-
-<body class="hold-transition sidebar-mini">
-  <div class="wrapper">
-
-    <?php include "includes/navbar.php"; ?>
-    <?php include "includes/sidebar.php"; ?>
-
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0 text-dark">VIP Inventory Management System</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                <li class="breadcrumb-item active">View Package # <?php echo $custID; ?></li>
-              </ol>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
-
-      <!-- Main content -->
-      <div class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="card">
-                <div class="card-header">
-                  <div class="d-flex justify-content-between">
-                    <h3 class="card-title">Package # <?php echo $custID; ?></h3>
-                    <a href="package-add.php">+ Add new package</a>
-                  </div>
-                </div>
-
-
-                <div class="card-body">
-                  <form  method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?custID=<?php echo $id; ?>">
-                  <div class="row">
-
-                    <div class="col-md-6">
-                       <div class="form-group">
-                      <label>Package Name</label>
-                      <input type="text" class="form-control" value="<?php echo $description; ?>" name="packName">
+        <!-- Main content -->
+        <div class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="card">
+                  <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                      <h3 class="card-title">Package # <?php echo $custID; ?></h3>
+                      <a href="package-add.php">+ Add new package</a>
                     </div>
+                  </div>
 
-                     <div class="form-group">
-                      <label>SKU</label>
-                      <input type="text" class="form-control" value="<?php echo $sku; ?>" name="sku">
+
+                  <div class="card-body">
+                    <form  method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?custID=<?php echo $id; ?>">
+                    <div class="row">
+
+                      <div class="col-md-6">
+                         <div class="form-group">
+                        <label>Package Name</label>
+                        <input type="text" class="form-control" value="<?php echo $description; ?>" name="packName">
                       </div>
 
-                      <label>Type</label>
-                      <input type='text' class='form-control' value="<?php echo $type; ?>" disabled>
+                       <div class="form-group">
+                        <label>SKU</label>
+                        <input type="text" class="form-control" value="<?php echo $sku; ?>" name="sku">
+                        </div>
+
+                        <label>Type</label>
+                        <input type='text' class='form-control' value="<?php echo $type; ?>" disabled>
+                      </div>
+
+                      <div class="col-md-6">
+                        <label>Date</label>
+                        <input type="text" class="form-control" value="<?php echo $created_at; ?>" disabled>
+                        <label>Created by</label>
+                        <input type="text" class="form-control" value="<?php echo $created_by; ?>" disabled>
+                      </div>
                     </div>
+                    <br>
+                    <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                      <thead>
+                        <tr>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">No.</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Package Model</th>
+                          <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Package Quantity</th>
 
-                    <div class="col-md-6">
-                      <label>Date</label>
-                      <input type="text" class="form-control" value="<?php echo $created_at; ?>" disabled>
-                      <label>Created by</label>
-                      <input type="text" class="form-control" value="<?php echo $created_by; ?>" disabled>
-                    </div>
-                  </div>
-                  <br>
-                  <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
-                    <thead>
-                      <tr>
-                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">No.</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Package Model</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Package Quantity</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                        // Attempt select query execution
+                        $query = "SELECT * FROM package_list WHERE packID = '$id' ";
+                        if($result = mysqli_query($link, $query)){
+                          if(mysqli_num_rows($result) > 0){
+                            $ctr = 0;
+                            while($row = mysqli_fetch_array($result)){
+                              $packlist = $row['pack_list_model']; 
+                              $packlistQty = $row['pack_list_qty'];
 
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php
-                      // Attempt select query execution
-                      $query = "SELECT * FROM package_list WHERE packID = '$id' ";
-                      if($result = mysqli_query($link, $query)){
-                        if(mysqli_num_rows($result) > 0){
-                          $ctr = 0;
-                          while($row = mysqli_fetch_array($result)){
-                            $packlist = $row['pack_list_model']; 
-                            $packlistQty = $row['pack_list_qty'];
-
-                            $ctr++;
-                            echo "<tr>";
-                            echo "<td>" . $ctr . "</td>";
-                            echo "<td><input type='text' class='form-control' value=' $packlist ' name='packlist'></td>";
-                            echo "<td><input type='text' class='form-control' value=' $packlistQty ' name='packlistQty'></td>";
-                            echo "</tr>";
-                            
-                           
+                              $ctr++;
+                              echo "<tr>";
+                              echo "<td>" . $ctr . "</td>";
+                              echo "<td><input type='text' class='form-control' value=' $packlist ' name='packlist[]'></td>";
+                              echo "<td><input type='text' class='form-control' value=' $packlistQty ' name='packlistQty[]'></td>";
+                              echo "</tr>";
+                              
+                             
+                            }
+                            // Free result set
+                            mysqli_free_result($result);
+                          } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
                           }
-                          // Free result set
-                          mysqli_free_result($result);
                         } else{
-                          echo "<p class='lead'><em>No records were found.</em></p>";
+                          echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
                         }
-                      } else{
-                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                      }
 
-                      // Close connection
-                      mysqli_close($link);
-                      ?>
-                    </tbody>
-                  </table>
+                        // Close connection
+                        mysqli_close($link);
+                        ?>
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div class="card-footer">
+                    <button type="submit" class="btn btn-primary" onclick="this.disabled=true;this.value='Submitting...'; this.form.submit();" >Save</button>
+                    </form>
                 </div>
-
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary" onclick="this.disabled=true;this.value='Submitting...'; this.form.submit();" >Save</button>
-                  </form>
               </div>
+
             </div>
-
+            <!-- /.row -->
           </div>
-          <!-- /.row -->
+          <!-- /.container-fluid -->
         </div>
-        <!-- /.container-fluid -->
+        <!-- /.content -->
       </div>
-      <!-- /.content -->
+      <!-- /.content-wrapper -->
+
+
+      <?php include "includes/footer.php"; ?>
     </div>
-    <!-- /.content-wrapper -->
+    <!-- ./wrapper -->
 
+    <?php include "includes/js.php"; ?>
 
-    <?php include "includes/footer.php"; ?>
-  </div>
-  <!-- ./wrapper -->
-
-  <?php include "includes/js.php"; ?>
-
-</body>
-</html>
+  </body>
+  </html>
