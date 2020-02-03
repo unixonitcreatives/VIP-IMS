761e0248ef1342711a4ee429cecdf015938eadb2
Joff <joffpascual@gmail.com>
2020-02-03 09:41:11 +0800

    Update invoice-unpaid-delete.php
    dasdasdas

--- invoice-unpaid-delete.php
+++ invoice-unpaid-delete.php
@@ -8,8 +8,8 @@ $query = "DELETE FROM outboundtb WHERE ob_tx_id= '$id' ";
 $res = mysqli_query($link, $query) or die(mysqli_error($link));
 if ($res){
      //echo "<script>alert('new staff added succesfully');</script>";
-                                    $info = $_SESSION['username']." deleted warehouse";
-                                    $info2 = "Details: ".$get_member_id;
+                                    $info = $_SESSION['username']." deleted invoice";
+                                    $info2 = "Details: ".$id;
                                     $alertlogsuccess = "Invoice has been deleted succesfully!";
                                     include('logs.php');
                                     echo "<script>window.location.href='invoice-unpaid.php';</script>";
